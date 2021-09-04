9ec90ded910762a3501cda1fff587f2e0f8392ae
Safayet Alam <safayetdib@gmail.com>
2021-09-04 22:17:18 +0600

    New UI

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -4,128 +4,132 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.calculatorapp.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
-    //to check if number and decimal point present
-    private var lastNumeric = false
-    private var lastDot = false
+    private var canAddOperation = false
+    private var canAddDecimal = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
 
-    fun onDigit(view: View) {
-        binding.tvInput.append((view as Button).text)
-        lastNumeric = true
-    }
-
-    fun onClear(view: View) {
-        binding.tvInput.text = ""
-        lastNumeric = false
-        lastDot = false
-    }
-
-    fun onDecimalPoint(view: View) {
-        if (lastNumeric && !lastDot) {
-            binding.tvInput.append(".")
-            lastDot = true
-            lastNumeric = false
-        }
-    }
-
-    fun onOperator(view: View) {
-        if (lastNumeric && !isOperatorAdded(binding.tvInput.text.toString())) {
-            binding.tvInput.append((view as Button).text)
-            lastDot = false
-            lastNumeric = false
-        }
-    }
-
-    private fun isOperatorAdded(value: String) : Boolean {
-        return if (value.startsWith("-")) {
-            false
-        } else {
-            value.contains("/") || value.contains("*")
-                    || value.contains("+") || value.contains("-")
-        }
-
-    }
-
-    fun onEqual(view: View) {
-        if (lastNumeric) {
-            var tvValue = binding.tvInput.text.toString()
-            var prefix = ""
-
-            try {
-                if (tvValue.startsWith("-")) {
-                    prefix = "-"
-                    tvValue = tvValue.substring(1)
-                }
-                 when {
-                    tvValue.contains("-") -> {
-                        val splitValue = tvValue.split("-")
-                        var one = splitValue[0]
-                        var two = splitValue[1]
-
-                        if (prefix.isNotEmpty()) {
-                            one = prefix + one
-                        }
-
-                        binding.tvInput.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
-                    }
-                    tvValue.contains("*") -> {
-                        val splitValue = tvValue.split("*")
-                        var one = splitValue[0]
-                        var two = splitValue[1]
-
-                        if (prefix.isNotEmpty()) {
-                            one = prefix + one
-                        }
-
-                        binding.tvInput.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
-                    }
-                    tvValue.contains("+") -> {
-                        val splitValue = tvValue.split("+")
-                        var one = splitValue[0]
-                        var two = splitValue[1]
-
-                        if (prefix.isNotEmpty()) {
-                            one = prefix + one
-                        }
-
-                        binding.tvInput.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
-                    }
-                    tvValue.contains("/") -> {
-                        val splitValue = tvValue.split("/")
-                        var one = splitValue[0]
-                        var two = splitValue[1]
-
-                        if (prefix.isNotEmpty()) {
-                            one = prefix + one
-                        }
-
-                        binding.tvInput.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
-                    }
-                }
-
-            } catch (e: ArithmeticException) {
-                e.printStackTrace()
+    fun numberAction(view: View) {
+        if (view is Button) {
+            if (view.text == ".") {
+                if (canAddDecimal) workingsTv.append(view.text)
+                canAddDecimal = false
+            } else {
+                workingsTv.append(view.text)
+                canAddOperation = true
             }
         }
     }
-
-    private fun removeZeroAfterDot(result: String): String {
-        var value = result
-        if (result.contains(".0")) {
-            value = result.substring(0, result.length - 2)
+    fun operationAction(view: View) {
+        if (view is Button && canAddOperation) {
+            workingsTv.append(view.text)
+            canAddOperation = false
+            canAddDecimal = true
         }
-        return value
+    }
+
+    fun allClearAction(view: View) {
+        workingsTv.text = ""
+        resultsTv.text = ""
+    }
+
+    fun backSpaceAction(view: View) {
+        val length = workingsTv.length()
+        if (length > 0) {
+            workingsTv.text = workingsTv.text.subSequence(0, length - 1)
+        }
+    }
+    fun equalsAction(view: View) {
+        resultsTv.text = calculateResults()
+    }
+
+    private fun calculateResults(): String {
+        val digitOperators = digitsOperators()
+        if (digitOperators.isEmpty()) return ""
+        
+        val timesDivision = timesDivisionCalculate(digitOperators)
+        if (timesDivision.isEmpty()) return ""
+
+        val result = addSubtractCalculate(timesDivision)
+        return result.toString()
+    }
+
+    private fun addSubtractCalculate(passedList: MutableList<Any>): Float {
+        var result = passedList[0] as Float
+
+        for (i in passedList.indices) {
+            if (passedList[i] is Char && i != passedList.lastIndex) {
+                val operator = passedList[i]
+                val nextDigit = passedList[i + 1] as Float
+                if (operator == '+') result += nextDigit
+                if (operator == '-') result -= nextDigit
+            }
+        }
+        return result
+    }
+
+    private fun timesDivisionCalculate(passedList: MutableList<Any>): MutableList<Any> {
+        var list = passedList
+        while (list.contains('x') || list.contains('/')) {
+            list = calcTimesDiv(list)
+        }
+        return list
+    }
+
+    private fun calcTimesDiv(passedList: MutableList<Any>): MutableList<Any> {
+        val newList = mutableListOf<Any>()
+        var restartIndex = passedList.size
+
+        for (i in passedList.indices) {
+            if (passedList[i] is Char && i != passedList.lastIndex && i < restartIndex) {
+                val operator = passedList[i]
+                val prevDigit = passedList[i - 1] as Float
+                val nextDigit = passedList[i + 1] as Float
+                when(operator) {
+                    'x' -> {
+                        newList.add(prevDigit * nextDigit)
+                        restartIndex = i + 1
+                    }
+                    '/' -> {
+                        newList.add(prevDigit / nextDigit)
+                        restartIndex = i + 1
+                    }
+                    else -> {
+                        newList.add(prevDigit)
+                        newList.add(operator)
+                    }
+                }
+            }
+            if (i > restartIndex) {
+                newList.add(passedList[i])
+            }
+        }
+
+        return newList
+    }
+
+    private fun digitsOperators(): MutableList<Any>{
+        val list = mutableListOf<Any>()
+        var currentDigit = ""
+        for (character in workingsTv.text) {
+            if (character.isDigit() || character == '.') {
+                currentDigit += character
+            } else {
+                list.add(currentDigit.toFloat())
+                currentDigit = ""
+                list.add(character)
+            }
+        }
+        if (currentDigit != "") list.add(currentDigit.toFloat())
+
+        return list
     }
 }
